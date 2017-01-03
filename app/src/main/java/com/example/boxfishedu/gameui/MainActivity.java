a1dae6cd6a61a61a7f7bcac4f021537d99c40130
TigerYao <yaohu201203@gmail.com>
2017-01-03 17:11:42 +0800

    animation

--- app/src/main/java/com/example/boxfishedu/gameui/MainActivity.java
+++ app/src/main/java/com/example/boxfishedu/gameui/MainActivity.java
@@ -2,17 +2,34 @@ package com.example.boxfishedu.gameui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.boxfishedu.gameui.ui.ClipSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Animation animation;
+    ImageView myImage;
     ClipSurfaceView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        view=new ClipSurfaceView(this);
-        setContentView(view);
+        setContentView(R.layout.activity_main);
+
+//        view=new ClipSurfaceView(this);
+//        setContentView(view);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.myanim);
+        myImage = (ImageView) this.findViewById(R.id.myImage);
+        myImage.startAnimation(animation);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        myImage.startAnimation(animation);
+        return super.onTouchEvent(event);
     }
 }
